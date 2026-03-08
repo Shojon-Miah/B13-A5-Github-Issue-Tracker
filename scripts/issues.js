170b4bd3c054c0054355186d9edbc1cbb99f8d50
Shojon-Miah <sujonv21@gmail.com>
2026-03-09 01:17:32 +0600

    Dynamic Loading Issues and Card Render

--- scripts/issues.js
+++ scripts/issues.js
@@ -1,1 +1,58 @@
 // issue loading, card rendering, modal logic will be implimented here
+
+// Loading All Issues= > API Fetch
+
+
+// Dynamic Issues Card Render
+
+
+async function loadIssues () {
+
+    try{
+        const response = await fetch("https://phi-lab-server.vercel.app/api/v1/lab/issues");
+        const result = await response.json();
+        // console.log(result);
+        
+        const issues = result.data;
+        const container = document.getElementById("issues-container");
+
+        container.innerHTML = "";
+
+        issues.forEach(issue => {
+
+            const card = document.createElement("div");
+            card.className = "border p-4 rounded-lg bg-white shadow";
+            
+            const labelsHTML = (issue.labels || [])
+            .map(label => `<span class ="text-xs bg-gray-200     px-2 py-1 rounded">${label}</span>`)
+            .join("");
+
+            card.innerHTML = `
+                <h3 class ="text-lg font-semibold">${issue.title}</h3>
+
+                <p class ="text-gray-600 mt-1">${issue.description}</p>
+            
+                <div class = flex gap-2 mt-3>${labelsHTML}</div>
+
+                <div class ="flex justify-between mt-4 text-sm text-gray-500">
+                    <span>Author: ${issue.author}</span>
+                    span>Priority: ${issue.priority}</span>
+                </div>
+
+            `;
+            
+            container.appendChild(card);
+
+        });
+
+
+    }
+    catch(error){
+
+        console.error("Failed to load issues:", error);
+
+    }
+    
+}
+
+loadIssues();
