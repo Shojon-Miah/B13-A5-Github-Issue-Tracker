5a5b05f43a0111ec08d8292a7879dad4abf68b4f
Shojon-Miah <sujonv21@gmail.com>
2026-03-08 06:07:06 +0600

    Form Submit Event

--- scripts/login.js
+++ scripts/login.js
@@ -1,1 +1,9 @@
 // Login related functionality will be implimented here 
+
+// Form Submit Event
+const loginForm = document.getElementById("login-form");
+loginForm.addEventListener("submit", function(event){
+    event.preventDefault();
+    
+    console.log("Login button clicked");
+});
