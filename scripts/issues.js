dac50b85bff87aed2188c4d4e66e61abaeca91c0
Shojon-Miah <sujonv21@gmail.com>
2026-03-09 08:28:22 +0600

    add Filter logic

--- scripts/issues.js
+++ scripts/issues.js
@@ -57,63 +57,21 @@ function renderIssues(issues) {
 }
 
 
+// Filter logic
 
+document.getElementById("tab-all").addEventListener("click", () => {
+    renderIssues(allIssues);
+});
 
-/*
-async function loadIssues () {
+document.getElementById("tab-open").addEventListener("click", () => {
+    const openIssues = allIssues.filter(issue => issue.status === "open");
+    renderIssues(openIssues);
+});
 
-    try{
-        const response = await fetch("https://phi-lab-server.vercel.app/api/v1/lab/issues");
-        const result = await response.json();
-        // console.log(result);
-        
-        const issues = result.data;
-        //
-        document.getElementById("issue-count").innerText = issues.length;
-        
-        const container = document.getElementById("issues-container");
+document.getElementById("tab-closed").addEventListener("click", () => {
+    const closedIssues = allIssues.filter(issue => issue.status === "open");
+    renderIssues(closedIssues);
+});
 
-        container.innerHTML = "";
-
-        issues.forEach(issue => {
-
-            const card = document.createElement("div");
-            card.className = "border p-4 rounded-lg bg-white shadow";
-            
-            const labelsHTML = (issue.labels || [])
-            .map(label => `<span class ="text-xs bg-gray-200 px-2 py-1 rounded">${label}</span>`)
-            .join("");
-
-            card.innerHTML = `
-                <h3 class ="text-lg font-semibold">${issue.title}</h3>
-
-                <p class ="text-gray-600 mt-1">${issue.description}</p>
-            
-                <div class = flex gap-2 mt-3>${labelsHTML}</div>
-
-                <div class ="flex justify-between mt-4 text-sm text-gray-500">
-                    <span>Author: ${issue.author}</span>
-                    span>Priority: ${issue.priority}</span>
-                </div>
-
-            `;
-            
-            container.appendChild(card);
-
-        });
-
-
-    }
-    catch(error){
-
-        console.error("Failed to load issues:", error);
-
-    }
-    
-}
-
-loadIssues();
-
-*/
 
 
